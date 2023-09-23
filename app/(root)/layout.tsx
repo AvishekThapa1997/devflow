d5e8a942a3a921a285af98df06695f6bcf5ac92d
AvishekThapa1997 <avishekthapa1997@gmail.com>
2023-09-23 21:45:38 +0530

    Add root folder structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
