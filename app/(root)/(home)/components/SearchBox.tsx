734d54a4b4b0eb86f90effb6ae98b16c7707cd6a
Avishek Thapa <avishek.thapa@mindfiresolutions.com>
2023-09-27 22:23:29 +0530

    Add props for search box

--- app/(root)/(home)/components/SearchBox.tsx
+++ app/(root)/(home)/components/SearchBox.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 import { Input } from '@app/(root)/components/ui/input';
 /* max-w-xl */
-export default function SearchBox() {
+interface SearchBoxProps {
+  placeholder: string;
+}
+export default function SearchBox({ placeholder }: SearchBoxProps) {
   return (
     <div className='relative w-full'>
       <div className='background-light800_darkgradient relative flex items-center rounded-lg px-4 py-0.5 shadow-sm dark:shadow-none'>
@@ -15,8 +18,9 @@ export default function SearchBox() {
         />
         <Input
           type='text'
-          placeholder='search'
-          className='paragraph-regular  da placeholder text-dark400_light700 no-focus border-none border-transparent bg-transparent shadow-none outline-none placeholder:capitalize focus-within:border-transparent'
+          placeholder={placeholder}
+          className='paragraph-regular text-dark400_light700 no-focus border-none border-transparent bg-transparent shadow-none outline-none 
+          focus-within:border-transparent'
         />
       </div>
     </div>
