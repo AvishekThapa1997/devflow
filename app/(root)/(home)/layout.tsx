92ffe5eae602889d0658303dbbe419fd2bec5757
Avishek Thapa <avishek.thapa@mindfiresolutions.com>
2023-09-28 12:56:39 +0530

    Fix layout during content overflow

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -7,14 +7,14 @@ import RightSidebar from './components/RightSidebar';
 export default function HomeLayout({ children }: BaseProps) {
   return (
     <div className='h-full'>
-      <div className='min-h-[10vh]'>
+      <div className='nav-container min-h-[4rem]'>
         <Navbar />
       </div>
-      <div className='flex min-h-[90vh] items-stretch'>
+      <div className='content-container flex'>
         <div className='hidden sm:block lg:w-72'>
           <LeftSidebar />
         </div>
-        <div className='flex-1 items-stretch xl:flex-[7]'>
+        <div className='flex-1 xl:flex-[7]'>
           <section className='flex flex-col px-6 pb-6 pt-6 max-md:pb-14 sm:px-14'>
             <div className='mx-auto w-full max-w-5xl'>{children}</div>
           </section>
